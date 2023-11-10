af4ee261df8665a98486e5122581eebd031bb00d
Yago <yarlorenzo@gmail.com>
2023-11-10 12:44:44 +0100

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
 module.exports = {
-  entry: './src/nurture.js',
+  entry: './src',
   mode: 'development',
   output: {
     path: path.resolve(__dirname, 'dist'),
